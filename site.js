e97e2c2d7cf19d04121b3423cec0c24cf5f8a779
Harry Cook <harry.cook@students.snow.edu>
2024-10-08 12:17:00 -0600

    finished part 1?

--- site.js
+++ site.js
@@ -1,21 +1,36 @@
 import { bookList } from "./books.js";
 
 
-const drawBooks = (books) => {
-  const bookHTML = (book) => `
-    <tr>
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td>${book.summary}</td>
-    </tr>
-  `
 
+
+
+
+const drawBooks = (books) => {
+  // const bookHTML = (book) =>
+  //    `
+  //   <tr>
+  //     <td>${book.title}</td>
+  //     <td>${book.author}</td>
+  //     <td>${book.summary}</td>
+  //   </tr>
+  // `
+  const generateBookNodes = (book) => {
+    const tableRowNode = document.createElement("tr")
+    const tableDataTitle = document.createElement("td")
+    const tableDataAuthor = document.createElement("td")
+    const tableDataSummary = document.createElement("td")
+    tableDataTitle.textContent = `${book.title}`
+    tableDataAuthor.textContent = `${book.author}`
+    tableDataSummary.textContent = `${book.summary}`
+    tableRowNode.replaceChildren(tableDataTitle,tableDataAuthor,tableDataSummary)
+    return tableRowNode;
+  }
   const tableBodyElement = document.getElementById("myTableBody")
-  tableBodyElement.innerHTML = "";
+  tableBodyElement.replaceChildren();
 
   books.forEach(book => {
-    const firstBookHtml = bookHTML(book)
-    tableBodyElement.innerHTML += firstBookHtml;
+    const BookNode = generateBookNodes(book)
+    tableBodyElement.appendChild(BookNode);
   })
 }
 
