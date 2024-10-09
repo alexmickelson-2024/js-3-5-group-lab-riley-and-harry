d7aa7c537eef4b19149fdc4fe3a16221ec1cad15
Harry Cook <harry.cook@students.snow.edu>
2024-10-09 09:46:59 -0600

    working on filtering

--- site.js
+++ site.js
@@ -3,24 +3,29 @@ import { bookList } from "./books.js";
 
 
 const filterText = document.getElementById("filterInput")
-filterText.addEventListener("input", (typedText)=> {
-    console.log(typedText)
+filterText.addEventListener("input", () => {
+  drawBooks(bookList, filterText.value);
+  console.log(filterText.value)
 })
 
 
-const drawBooks = (books) => {
+const drawBooks = (books, filterTextContent) => {
   const generateBookNodes = (book) => {
     const tableRowNode = document.createElement("tr")
     const tableDataTitle = document.createElement("td")
     const tableDataAuthor = document.createElement("td")
     const tableDataSummary = document.createElement("td")
+    tableDataTitle.classList.add("title")
+    tableDataAuthor.classList.add("author")
+    tableDataSummary.classList.add("sumarry")
     tableDataTitle.textContent = `${book.title}`
     tableDataAuthor.textContent = `${book.author}`
     tableDataSummary.textContent = `${book.summary}`
-    tableRowNode.replaceChildren(tableDataTitle,tableDataAuthor,tableDataSummary)
+    tableRowNode.replaceChildren(tableDataTitle, tableDataAuthor, tableDataSummary)
     tableRowNode.addEventListener("click", (click) => {
       document.getElementById("selectedBookTitle").textContent = `${tableDataTitle.textContent}`
     })
+
     return tableRowNode;
   }
   const tableBodyElement = document.getElementById("myTableBody")
@@ -28,7 +33,10 @@ const drawBooks = (books) => {
 
   books.forEach(book => {
     const BookNode = generateBookNodes(book)
-    tableBodyElement.appendChild(BookNode);
+    if (true) {
+      tableBodyElement.appendChild(BookNode);
+    }
+    console.log(BookNode.getElementsByTagName("td")[0])
   })
 
 }
